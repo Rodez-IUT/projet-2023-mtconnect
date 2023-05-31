f10a7c8c724bfba12a5c5886f2a8ac094b99f66f
Clement Bonaz <clement.bonaz@iut-rodez.fr>
2023-05-31 08:47:29 +0200

    fusion de la branche modifier ordre bouton

--- MTConnectAgent/ConsoleApp1/Program.cs
+++ MTConnectAgent/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
 
             MTConnectClient mTConnectClient = new MTConnectClient();
